d4b0eece95f8d7f6b370fdbbdccd851f58b73099
jasonbai <jasonbai@noreply.gitcode.com>
2024-08-07 17:23:49 +0800

    新增国债指数

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,18 @@
   <div class="hello">
     <div class="wrap" @dblclick="refresh">
       <template v-for="(item, i) in dataArr">
-        <!-- 单独设置十年期国债收益率 -->
-        <div v-if="i === 0" :key="i" :class="{ up: item[4] < 0, down: item[4] > 0, item: true }" @click="showChart">
+        <!-- 单独设置国债指数 -->
+        <div v-if="i === 0" :key="i" :class="{ up: item[4] > 0, down: item[4] < 0, item: true }" @click="showChart">
           <h4>
-            {{ item[1] }} <span>{{ item[4] > 0 ? "↑" : "↓" }}</span>
+            {{ item[1] }} <span>{{ item[2].toFixed(2) }}</span>
           </h4>
-          <span class="rate">{{ item[2] }}</span>
+          <span class="rate">{{ item[4].toFixed(2) + " %" }}</span>
         </div>
         <!-- 设置其余指数 -->
         <div v-else :key="i + 'x'" :class="{ up: item[4] > 0, down: item[4] < 0, item: true }">
-          <h4 v-if="item[0] === '000001.SS'">{{ item[1] }} {{ parseInt(item[2]) }}</h4>
+          <h4 v-if="item[0] === '000001.SH'">{{ item[1] }} {{ parseInt(item[2]) }}</h4>
           <h4 v-else>{{ item[1] }}</h4>
-          <span class="rate" v-if="item[4] > 0">{{ item[4].toFixed(2) + "%" }}</span>
+          <span class="rate" v-if="item[4] > 0">{{ item[4].toFixed(2) + " %" }}</span>
         </div>
       </template>
     </div>
@@ -49,9 +49,17 @@ export default {
         });
     },
     // 从华尔街见闻获取主要股指的涨跌幅数据
+    // CN10YR.OTC: 十年期国债收益率
+    // 000012.SH: 国债指数
+    // 000001.SH: 上证指数
+    // 399006.SZ: 创业板
+    // 399300.SZ: 沪深300
+    // 399905.SZ: 中证500
+    // 000852.SS: 中证1000
+    // US500.OTC: 标普500
     getDataFromWallstreet() {
       let stockIndex = "symbol,en_name,prod_name,last_px,px_change,px_change_rate,update_time";
-      let indexStr = "CN10YR.OTC,000001.SS,399006.SZ,399300.SZ,399905.SZ,000852.SS,US500.OTC";
+      let indexStr = "000012.SH,000001.SH,399006.SZ,399300.SZ,399905.SZ,000852.SS,US500.OTC";
       return fetch(`https://api-ddc-wscn.awtmt.com/market/real?fields=${stockIndex}&prod_code=${indexStr}`).then((response) => response.json());
     },
     getData() {
@@ -60,17 +68,17 @@ export default {
         let data1 = resArr[0];
         // 纳斯达克股指数据
         let data2 = resArr[1];
-        // 存储十年期国债收益率
-        let tenYear = JSON.parse(JSON.stringify(data1.data.snapshot["CN10YR.OTC"]));
-        // 删除十年期国债收益率
-        delete data1.data.snapshot["CN10YR.OTC"];
+        // 存储国债指数
+        let tenYear = JSON.parse(JSON.stringify(data1.data.snapshot["000012.SH"]));
+        // 删除国债指数
+        delete data1.data.snapshot["000012.SH"];
         // 提取成二维数组
         let origin = Object.values(data1.data.snapshot);
         // 融合纳斯达克数据
         let dataAll = [...origin, data2];
         // 根据涨跌幅排序
         this.dataArr = dataAll.sort((a, b) => b[4] - a[4]);
-        // 在头部插入十年期国债收益率
+        // 在头部插入国债指数
         this.dataArr.unshift(tenYear);
         // 修改标题
         setTimeout(() => (document.title = "韭菜看盘"), 500);
@@ -122,7 +130,7 @@ export default {
     h4 {
       font-weight: normal;
       span {
-        vertical-align: top;
+        vertical-align: middle;
       }
     }
     span.rate {
